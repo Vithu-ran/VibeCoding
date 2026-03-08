7a06725a2a05937679f854d27971cd85bb95e2f7
Vithuran <v.vithuranvithu@gmail.com>
2026-03-08 20:17:36 +0530

    Modified the output format

--- GradeCalculator.py
+++ GradeCalculator.py
@@ -12,16 +12,19 @@ def main():
             
             average = (maths + science + english) / 3
             
-            print(f"Student Name: {name}")
+            print("-" * 17)
+            print(f"Name : {name}")
+            print(f"Average : {average:.1f}")
             
             if average >= 75:
-                print("Grade: A")
+                print("Grade : A")
             elif average >= 60:
-                print("Grade: B")
+                print("Grade : B")
             elif average >= 40:
-                print("Grade: C")
+                print("Grade : C")
             else:
-                print("Grade: Fail")
+                print("Grade : Fail")
+            print("-" * 17)
                 
         except ValueError:
             print("Invalid input. Please enter numerical values for marks.")
